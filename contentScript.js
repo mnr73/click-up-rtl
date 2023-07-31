2ed6550c0ec66b2fcc245cfea6eb9245012398a8
mnr73 <mnrmilad@gmail.com>
2023-08-01 02:38:19 +0330

    don't add style if style added before

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,16 @@
 (() => {
 	chrome.runtime.onMessage.addListener((obj, sender, response) => {
 		console.log('new styles applied');
-		document.head.insertAdjacentHTML("beforeend", `<style>
-		ql-rendered-list-container,
-		cu-slash-command,
-		cu-task-editor .ql-block,
-		.cu-task-row-main__link-text-inner,
-		.cu-panel-board__clickable-name
-		{direction:rtl;unicode-bidi:isolate}
-		</style>`)
+		if(document.querySelector('style[name="click-up-rtl-custom-style"]') == null){
+			document.head.insertAdjacentHTML("beforeend", `<style name='click-up-rtl-custom-style'>
+			ql-rendered-list-container,
+			cu-slash-command,
+			cu-task-editor .ql-block,
+			.cu-task-row-main__link-text-inner,
+			.cu-panel-board__clickable-name
+			{direction:rtl;unicode-bidi:isolate}
+			</style>`)
+		}
 
 	})
 })();
